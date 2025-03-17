27c123a153518b22bfdd9e92eb16479e40b49d48
haewonuhm <huhm@andrew.cmu.edu>
2025-03-16 23:59:02 -0400

    Update avgTemps.py

--- avgTemps.py
+++ avgTemps.py
@@ -16,37 +16,49 @@ t = sm_tc.SMtc(0)
 
 def avgTemps():
     if "ch1":
-        for len(tempReadings1) in range(11):
+        counter1 = 0
+        while True:
             tempReadings1.append(tempVal1)
-            if len(tempReadings1) or len> numReadings: 
+            if len(tempReadings1) > numReadings: 
                 tempReadings1.pop(0)
-            avgVal1 = sum(tempReadings1) / len(tempReadings1)
-            return(avgVal1)
+            counter1 += 1
+            if counter1 // 5 == 0: 
+                avgVal1 = sum(tempReadings1) / len(tempReadings1)
+                return(avgVal1)
             time.sleep(1.0)
 
     if "ch2":
-        for len(tempReadings2) in range(11):
+        counter2 = 0
+        while True:
             tempReadings2.append(tempVal2)
-            if len(tempReadings2) or len> numReadings: 
+            if len(tempReadings2) > numReadings: 
                 tempReadings2.pop(0)
-            avgVal2 = sum(tempReadings2) / len(tempReadings2)
-            return(avgVal2)
+            counter2 += 1
+            if counter2 // 5 == 0:
+                avgVal2 = sum(tempReadings2) / len(tempReadings2)
+                return(avgVal2)
             time.sleep(1.0)
 
-     if "ch3":
-        for len(tempReadings3) in range(11):
+    if "ch3":
+        counter3 = 0
+        while True:
             tempReadings3.append(tempVal3)
-            if len(tempReadings3) or len> numReadings: 
+            if len(tempReadings3) > numReadings: 
                 tempReadings3.pop(0)
-            avgVal3 = sum(tempReadings3) / len(tempReadings3)
-            return(avgVal3)
+            counter3 += 1
+            if counter3 // 5 == 0: 
+                avgVal3 = sum(tempReadings3) / len(tempReadings3)
+                return(avgVal3)
             time.sleep(1.0)
     
     if "ch4":
-        for len(tempReadings4) in range(11):
+        counter4 = 0
+        while True:
             tempReadings4.append(tempVal4)
-            if len(tempReadings4) or len> numReadings: 
+            if len(tempReadings4) > numReadings: 
                 tempReadings4.pop(0)
-            avgVal4 = sum(tempReadings4) / len(tempReadings4)
-            return(avgVal4)
+            counter4 += 1
+            if counter4 // 5 == 0:
+                avgVal4 = sum(tempReadings4) / len(tempReadings4)
+                return(avgVal4)
             time.sleep(1.0)
