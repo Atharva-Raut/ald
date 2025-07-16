133aab85a94f43be6851169256441407b1eccbba
Brandon Joel Gonzalez <bgonzale@andrew.cmu.edu>
2025-07-16 08:10:48 -0400

    Set up command line parsing

--- controls/command_parser/command_parser.ino
+++ controls/command_parser/command_parser.ino
@@ -6,6 +6,7 @@
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("Waiting for job...");
 }
 
 void loop()
@@ -24,6 +25,28 @@ void loop()
   // (IMPORTANT! The pulses will continue one after another,
   // so we will see valve 1 -> valve 2 -> valve 3 with purging in between each.)
   // Then, thermocouple 1 is set at 300.0C. TC2 at 180.0C. TC3 at 120.0C. And TC4 at 250.0C.
+  // Note that a job must have data for every field.
 
-  // This files is responsible for parsing commands appropriately as described above.
+  if (Serial.available() > 0) { // Check if data is available in the serial buffer
+    char s[100] = {0};
+    String inputString = Serial.readStringUntil('\n'); // Read until newline character
+    strcpy(s, inputString.c_str());
+    
+    // s = "8,500.0,12,200.0,4,1200.0,300.0,180.0,120.0,250.0";
+
+    float f1, f2, f3, f4, f5, f6, f7, f8, f9, f10;
+
+    int result = sscanf(s, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f", &f1, &f2 ,&f3, &f4, &f5, &f6, &f7, &f8, &f9, &f10);
+
+    // unable to parse command-line input
+    if (result != 10)
+    {
+      Serial.println("BAD JOB");
+      return;
+    }
+
+    Serial.print("You entered: ");
+    Serial.println(s);
+  }
+
 }
